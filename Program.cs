29c5d067829b1ad7a1c2bc1f649281fbfd9614f5
LucianoBonilla <lucianobonilla27@gmail.com>
2023-06-08 18:34:44 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using tareas;
+int a;
+
+Console.WriteLine("Ingrese la cantidad de tareas: ");
+int.TryParse(Console.ReadLine(), out a);
+
+for (int i = 0; i < a; i++)
+{
+    
+}
+
+/* using System;
+ 
+namespace prueba
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+ 
+            Random r= new Random ();
+ 
+            //Genera un numero entre 10 y 100 (101 no se incluye)
+            for (int i = 0; i <5; i++)
+            {   
+                Console.WriteLine(r.Next (10,101));
+            }
+ 
+            Console.ReadLine ();
+        }
+ 
+    }
+} */
---
+++ clase.cs
@@ -0,0 +1,14 @@
+namespace tareas;
+
+public class tarea {
+    private int tareaID;
+    private string? descripcion;
+    private int duracion;
+
+    public int TareaID { get => tareaID; set => tareaID = value; }
+
+    private string[] descripciones = {"Limpiar el piso", "Lavar los platos", "Cocinar"};
+
+    
+}
+
